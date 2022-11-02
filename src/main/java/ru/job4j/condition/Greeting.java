a9e9bf9e5d4b5e4bb11b47b5be2f4a721f64b8dc
Morfyus <Sokol_70@tut.by>
2022-11-02 18:37:10 +0300

    9.1. String [#237407 #98489]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+String idea = "I ";
+idea = idea+"like ";
+idea += "Java!";
+String condition = " but I am a newbie";
+String result = idea + condition;
+System.out.println (result);
+int year = 2022;
+String separate = " ";
+String rsl = idea + separate + year;
+System.out.println(rsl);
+}
+    }
+
